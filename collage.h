f58f61664d0e4924a97fdba717d0613b48b9c532
DipshikhaDas <98797569+DipshikhaDas@users.noreply.github.com>
2022-03-04 01:20:37 +0600

    Add files via upload

---
+++ collage.h
@@ -0,0 +1,6 @@
+#ifndef COLLAGE_H_INCLUDED
+#define COLLAGE_H_INCLUDED
+
+int to_binary();
+
+#endif // COLLAGE_H_INCLUDED
---
+++ main.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+#include "collage.h"
+using namespace std;
+
+int main()
+{
+
+    to_binary();
+    return 0;
+}
